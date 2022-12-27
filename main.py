a82f2db810edb6a107391701cb42c025f9f1d253
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-27 13:32:20 +0100

    Added new features to webpage

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, DecimalField
+from wtforms.validators import DataRequired, NumberRange
 import requests
+
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_API_KEY = "12345"   # your API key here
 
 app = Flask(__name__)
 app.app_context().push()
@@ -17,9 +20,14 @@ db = SQLAlchemy(app)
 
 
 class RateMovieForm(FlaskForm):
-    rating = StringField("Your Rating Out of 10 e.g. 7.5", validators=DataRequired)
-    review = StringField("Your Review", validators=DataRequired)
+    rating = DecimalField("Your Rating Out of 10 e.g. 7.5", validators=[DataRequired(), NumberRange(min=0, max=10)])
+    review = StringField("Your Review", validators=[DataRequired()])
     submit = SubmitField("Done")
+
+
+class AddMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
 
 
 class Movie(db.Model):
@@ -35,18 +43,6 @@ class Movie(db.Model):
 
 db.create_all()
 
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# db.session.add(new_movie)
-# db.session.commit()
-
 
 @app.route("/")
 def home():
@@ -54,13 +50,56 @@ def home():
     return render_template("index.html", movies=all_movies)
 
 
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    add_form = AddMovieForm()
+    if add_form.validate_on_submit():
+        movie_title = add_form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=add_form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            #The data in release_date includes month and day, we will want to get rid of.
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("edit_movie"))
+
+
 @app.route("/edit", methods=["GET", "POST"])
-def edit():
-    if request.method == "POST":
-        pass
+def rate_movie():
     movie_id = request.args.get("id")
-    movie_selected = Movie.query.get(movie_id)
-    return render_template("edit.html", movie=movie_selected)
+    movie = Movie.query.get(movie_id)
+    rate_form = RateMovieForm()
+    if rate_form.validate_on_submit():
+        movie.rating = float(rate_form.rating.data)
+        movie.review = rate_form.review.data
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", movie=movie, form=rate_form)
+
+
+@app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
